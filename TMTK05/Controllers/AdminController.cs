3df3b721690b16afddbc950a93cd0ca23f68de4d
Maarten, Owain, Zowie <owain.vanbrakel@gmail.com>
2014-09-26 09:33:35 +0200

    Initial commit

---
+++ TMTK05/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMTK05.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
