da4295131f19bcd884466935af95698e4dc9f4bb
Owain van Brakel <Owain.vanbrakel@gmail.com>
2014-09-28 04:52:49 +0200

    Add identity model

---
+++ TMTK05/Models/IdentityModel.cs
@@ -0,0 +1,54 @@
+﻿#region
+
+using System;
+using System.Globalization;
+using System.Web;
+using System.Web.Security;
+
+#endregion
+
+namespace TMTK05.Models
+{
+    public static class IdentityModel
+    {
+        #region Public Properties
+
+        public static bool CurrentUserOwner
+        {
+            get
+            {
+                if (!HttpContext.Current.Request.IsAuthenticated) return false;
+                var user = HttpContext.Current.User.Identity as FormsIdentity;
+                // ReSharper disable PossibleNullReferenceException 
+                var ticket = user.Ticket;
+                // ReSharper restore PossibleNullReferenceException 
+                return ticket.UserData.Split('|')[1] == "1";
+            }
+        }
+
+        public static string CurrentUserId
+        {
+            get
+            {
+                var userId = String.Empty;
+
+                if (HttpContext.Current.Request.IsAuthenticated)
+                {
+                    userId = HttpContext.Current.User.Identity.Name.Split('|')[0];
+                }
+
+                return userId.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        public static bool CurrentUserLoggedIn
+        {
+            get
+            {
+                return HttpContext.Current.Request.IsAuthenticated;
+            }
+        }
+
+        #endregion Public Properties
+    }
+}
