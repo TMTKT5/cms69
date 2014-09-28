2b644b0f45ea075a3defdc9de6813cbd5d319abb
Owain van Brakel <Owain.vanbrakel@gmail.com>
2014-09-28 07:03:45 +0200

    Captalize the first letter of the username

--- TMTK05/Classes/StringManipulation.cs
+++ TMTK05/Classes/StringManipulation.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Linq;
 
 #endregion
 
@@ -52,6 +53,23 @@ namespace TMTK05.Classes
                 }
             }
             return new string(output);
+        }
+
+        /// <summary>
+        /// Captalize first letter of a string
+        /// </summary>
+        public static string CaptalizeFirstLetter(this string data)
+        {
+            var chars = data.ToCharArray();
+
+            // Find the Index of the first letter
+            var charac = data.First(char.IsLetter);
+            var i = data.IndexOf(charac);
+
+            // capitalize that letter
+            chars[i] = char.ToUpper(chars[i]);
+
+            return new string(chars);
         }
     }
 }
