90fff4a948e44ea49750810d69f6f9263b0250f2
Maarten Paauw <maartenpaauw@gmail.com>
2014-10-04 23:18:07 +0200

    Started the Add Page page.

---
+++ TMTK05/Models/NewPageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TMTK05.Models
+{
+    public class NewPageModel
+    {
+        [Display(Name = "Title:")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description:")]
+        public string Description { get; set; }
+
+        [Display(Name = "Content:")]
+        public string Content { get; set; }
+
+        [Display(Name = "Image:")]
+        public string Image { get; set; }
+    }
+}
