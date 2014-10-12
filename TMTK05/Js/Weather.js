7dd08910ed608d252f49f9a37cd964227577e018
Owain van Brakel <Owain.vanbrakel@gmail.com>
2014-10-12 08:24:18 +0200

    Alt temp is in Fahrenheit not in Celsius

--- TMTK05/Js/Weather.js
+++ TMTK05/Js/Weather.js
@@ -19,7 +19,7 @@ function loadWeather(location, woeid) {
             html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
             html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
             html += '<li class="currently">'+weather.currently+'</li>';
-            html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+            html += '<li>'+weather.alt.temp+'&deg;F</li></ul>';  
       
             $("#weather").html(html);
         },
