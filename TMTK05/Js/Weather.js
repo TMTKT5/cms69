90a1da5f514366b0a6a4c64268caf8508d302797
Owain van Brakel <Owain.vanbrakel@gmail.com>
2014-10-12 08:14:58 +0200

    Html5 geolocation for weather keep Rotterdam as default

--- TMTK05/Js/Weather.js
+++ TMTK05/Js/Weather.js
@@ -1,17 +1,25 @@
-﻿$(document).ready(function () {
-    $.simpleWeather({
-        location: 'Rotterdam, ZH',
-        woeid: '73307',
-        unit: 'c',
-        success: function (weather) {
-            html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-            html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
-            html += '<li class="currently">' + weather.currently + '</li>';
-
-            $("#weather").html(html);
-        },
-        error: function (error) {
-            $("#weather").html('<p>' + error + '</p>');
-        }
+﻿$(document).ready(function() {
+    loadWeather('Rotterdam, ZH','');
+    navigator.geolocation.getCurrentPosition(function(position) {
+        loadWeather(position.coords.latitude+','+position.coords.longitude);
     });
 });
+
+function loadWeather(location, woeid) {
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: 'c',
+        success: function(weather) {
+            html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+            html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+            html += '<li class="currently">'+weather.currently+'</li>';
+            html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
+            $("#weather").html(html);
+        },
+        error: function(error) {
+            $("#weather").html('<p>'+error+'</p>');
+        }
+    });
+}
